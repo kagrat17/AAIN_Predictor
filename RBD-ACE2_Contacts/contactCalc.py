8453b9f143c7e34af84086ea1def3363b2f1b70f
kagrat17 <kagrat7@gmail.com>
2022-02-06 17:15:36 -0500

    computed contact data for variants

--- RBD-ACE2_Contacts/contactCalc.py
+++ RBD-ACE2_Contacts/contactCalc.py
@@ -7,10 +7,10 @@ from Bio.PDB import *
 pdbFile = input("Enter the PDB file to calculate contacts for (exclude file extension): ")
 
 cwd = os.getcwd()
-f = open(cwd + "/Contact_Data/" + pdbFile + "_Contacts", mode="a")
+f = open(cwd + "/RBD-ACE2_Contacts/Contact_Data/" + pdbFile + "_Contacts.txt", mode="w")
 parser = PDBParser(PERMISSIVE=True, QUIET=True)
-data = parser.get_structure(pdbFile, cwd + "/../PDB_Files/" + pdbFile + ".pdb")
-model = data.get_models()
+struct = parser.get_structure(pdbFile, cwd + "/PDB_Files/" + pdbFile + ".pdb")
+model = struct.get_models()
 
 models = list(model)
 chains = list(models[0].get_chains())
@@ -48,7 +48,7 @@ for i in range(len(cAlphaE[0])):
             count += 1
             countTot += 1
             contacts += cAlphaA[0][j] + " " + \
-                str(int((cAlphaE[1][i] - cAlphaA[1][j])*100)/100) + "\n"
+                str(int((cAlphaE[1][i] - cAlphaA[1][j])*1000)/1000) + "\n"
     contacts = contacts[:len(contacts)-2]
     if count > 0:
         data[0].append(cAlphaE[0][i])
@@ -69,18 +69,17 @@ for i in range(len(data[0])):
     arr = [str(data[0][i]) + "(" + str(data[1][i]) + ")", data[2][i]]
     dataInRows.append(arr)
 
-f.write("Total Contacts: " + str(countTot), end="\n")
-f.write("Cutoff Distance: 7 Angstroms", end="\n")
-f.write("\n")
-f.write(str(len(data[0])) + " RBD Contact Residues:", end="\n")
+f.write(pdbFile + ": " + struct.header["name"] + "\n\n")
+f.write("Total Contacts: " + str(countTot) + "\n")
+f.write("Cutoff Distance: 7 Angstroms" + "\n\n")
+f.write(str(len(data[0])) + " RBD Contact Residues: ")
 for i in range(len(data[0])):
-    f.write(str(data[0][i]) + "(" + str(data[1][i]) + ")", end="\n")
+    f.write(str(data[0][i]) + "(" + str(data[1][i]) + ") ")
 f.write("\n")
-f.write(str(len(aceRes)) + " ACE2 Contact Residues:", end="\n")
+f.write(str(len(aceRes)) + " ACE2 Contact Residues: ")
 for c in aceRes:
-    f.write(str(c.split(" ")[0]) + "(" + str(aceRes[c]) + ")", end="\n")
-f.write("\n")
-f.write("\n")
+    f.write(str(c.split(" ")[0]) + "(" + str(aceRes[c]) + ") ")
+f.write("\n\n")
 f.write(tabulate((dataInRows), headers=["RBD Residue", "ACE2 Residues"]))
 
 f.flush()
